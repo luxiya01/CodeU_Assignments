08a0ae645e129a2a242a51d8c8518b745560818b
Li <liling@kth.se>
2017-05-20 01:15:02 +0200

    Empty strings are permutations of each other

--- assignment1/string_permutation.py
+++ assignment1/string_permutation.py
@@ -1,10 +1,10 @@
 import string 
 
 def string_permutation(str1, str2):
-    if str.isalpha(str1 and str2):
+    if (str.isalpha(str1) or str1 == "") and (
+            str.isalpha(str2) or str2 == ""):
         str1 = str1.lower()
         str2 = str2.lower()
-        result = True
         inputs = {x: 0 for x in string.ascii_lowercase} 
         for c in str1: 
             inputs[c] += 1
@@ -12,9 +12,7 @@ def string_permutation(str1, str2):
             inputs[c] -= 1
         for c in inputs.keys():
             if inputs[c] != 0:
-                result = False
-                break
-        return result
+                return False
+        return True
     else: 
         raise ValueError("The inputs strings shall only consist of English alphabets")
-
