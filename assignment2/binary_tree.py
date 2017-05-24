44f2f060a810af9b1348b46744613b7b81aec9ef
Li <liling@kth.se>
2017-05-24 16:18:58 +0200

    Add Node class

---
+++ assignment2/binary_tree.py
@@ -0,0 +1,10 @@
+class Node(object):
+    """Defines one node in the binary tree. 
+
+    Attributes: key, left node, right node. """
+
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
