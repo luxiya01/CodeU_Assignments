c625afb1ad7d41486f5c4125e91025601c6d7276
Li <liling@kth.se>
2017-06-10 18:51:19 +0200

    Add docstrings for functions

--- assignment3/word_search.py
+++ assignment3/word_search.py
@@ -6,6 +6,18 @@ WORD_DICT = None
 CHAR_2D_ARRAY = None
 
 def findAllWords(row, col, char2dArray, wordDict):
+    """Find all valid words that can be formed in the char2dArray. 
+    Note that CHAR_2D_ARRAY, WORD_DICT and ALL_WORDS are global variables. 
+
+    Args:
+        row: no.rows in char2dArray
+        col: no.col in char2dArray
+        char2dArray: the grid of letters where words are to be searched for
+        wordDict: a dictionary containing methods isWord(string) and isPrefix(string)
+
+    Returns: 
+        ALL_WORDS: a set containing all valid words found. 
+    """
     global CHAR_2D_ARRAY, WORD_DICT
     CHAR_2D_ARRAY = char2dArray
     WORD_DICT = wordDict
@@ -19,6 +31,17 @@ def findAllWords(row, col, char2dArray, wordDict):
     return ALL_WORDS
 
 def findAllWordsFromCell(i, j, curr, visited):
+    """Find all words that begins with curr. 
+
+    Args:
+        i: row index of the current cell
+        j: col index of the current cell
+        curr: the current prefix string
+        visited: all cells visited when forming curr
+
+    Returns: 
+        Nothing. Note that ALL_WORDS is modified on the go. 
+    """
     if WORD_DICT.isWord(curr):
         ALL_WORDS.add(curr)
     if i > 0:
@@ -32,6 +55,17 @@ def findAllWordsFromCell(i, j, curr, visited):
     checkLeftRight(i, j, curr, visited)
 
 def checkLeftRight(i, j, curr, visited):
+    """Check cells to the left and right of CHAR_2D_ARRAY[i][j]. 
+
+    Args:
+        i: row index of the current cell
+        j: col index of the current cell
+        curr: the current prefix string
+        visited: all cells visited when forming curr
+
+    Returns: 
+        Nothing. Note that ALL_WORDS is modified on the go. 
+    """
     if j > 0:
         newCurr = curr + CHAR_2D_ARRAY[i-1][j-1]
         checkCell(i, j-1, newCurr, visited)
@@ -39,8 +73,20 @@ def checkLeftRight(i, j, curr, visited):
         newCurr = curr + CHAR_2D_ARRAY[i][j+1]
         checkCell(i, j+1, newCurr, visited)
 
-
 def checkCell(i, j, curr, visited): 
+    """Check cell CHAR_2D_ARRAY[i][j]. 
+    If CHAR_2D_ARRAY[i][j] is unvisited and curr is a valid prefix, 
+    recursively call findAllWordsFromCell from cell CHAR_2D_ARRAY[i][j]. 
+
+    Args:
+        i: row index of the current cell
+        j: col index of the current cell
+        curr: the current prefix string
+        visited: all cells visited when forming curr
+
+    Returns: 
+        Nothing. Note that ALL_WORDS is modified on the go. 
+    """
     if (i, j) not in visited and WORD_DICT.isPrefix(curr):
         visited.add((i, j))
         findAllWordsFromCell(i, j, curr, visited)
