8ac056ddea084954eb15af2e6e2d5421f6a8b3d3
Li <liling@kth.se>
2017-07-15 00:11:54 +0200

    Modify islandCount according to reviews received

--- assignment4/islandCount.py
+++ assignment4/islandCount.py
@@ -1,7 +1,8 @@
 # Author: Li Ling
 # Last modified: 2017-06-25
+import copy
 
-class IslandCounter(object):
+class IslandCounter():
     """Used to count the amount of islands in a 2d landscape
     of water and land.
 
@@ -27,31 +28,26 @@ class IslandCounter(object):
             number of islands in the original landscape.
         """
         islands = 0
-        modified_landscape = self.landscape
+        modified_landscape = copy.deepcopy(self.landscape)
         for i in range(self.row):
             for j in range(self.col):
                 if modified_landscape[i][j]:
-                    self.update_visited_tiles(modified_landscape, i, j)
+                    self._update_visited_tiles(modified_landscape, i, j)
                     islands += 1
         return islands
 
-    def update_visited_tiles(self, modified_landscape, i, j):
+    def _update_visited_tiles(self, modified_landscape, i, j):
         """Update the modified_landscape by recursive flood filling method."""
-        curr_island = list()
+        island = list()
         modified_landscape[i][j] = False
-        curr_island.append((i, j))
-        while curr_island:
-            curr_tile = curr_island.pop()
-            curr_i, curr_j = curr_tile[0], curr_tile[1]
-            if curr_i > 0 and modified_landscape[curr_i-1][curr_j]:
-                modified_landscape[curr_i-1][curr_j] = False
-                curr_island.append((curr_i-1, curr_j))
-            if curr_i < self.row-1 and modified_landscape[curr_i+1][curr_j]:
-                modified_landscape[curr_i+1][curr_j] = False
-                curr_island.append((curr_i+1, curr_j))
-            if curr_j > 0 and modified_landscape[curr_i][curr_j-1]:
-                modified_landscape[curr_i][curr_j-1] = False
-                curr_island.append((curr_i, curr_j-1))
-            if curr_j < self.col-1 and modified_landscape[curr_i][curr_j+1]:
-                modified_landscape[curr_i][curr_j+1] = False
-                curr_island.append((curr_i, curr_j+1))
+        island.append((i, j))
+        while island:
+            i, j = island.pop()
+            for i_offset, j_offset in [(-1, 0), (1, 0), (0, 1), (0, -1)]:
+                if self._is_unvisited_land_tile(modified_landscape, i + i_offset, j + j_offset):
+                    modified_landscape[i + i_offset][j + j_offset] = False
+                    island.append((i + i_offset, j + j_offset))
+
+
+    def _is_unvisited_land_tile(self, modified_landscape, i, j):
+        return 0 <= i < self.row and 0 <= j < self.col and modified_landscape[i][j]
